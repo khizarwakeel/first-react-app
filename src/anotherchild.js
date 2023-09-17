1306f2994046ae9b92065f3cdce702a306905d52
Khizar Wakeel <khizarwakeel@gmail.com>
2023-09-17 17:10:51 +0500

    Successfully deploy on surge using Github Actions.

--- src/anotherchild.js
+++ src/anotherchild.js
@@ -4,6 +4,7 @@ function Anotherchild({name}) {
   return (
     <div>
       <h1>Hello this is another child {name}</h1>
+      <h1>Allah ka Shukar hai I have successfully deploy my React app using Git Hub Actions on Surge</h1>
     </div>
   )
 }
